7cd866e4956355d20f398003fc2fef28f544740f
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-17 15:49:33 -0400

    updated left side bar

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,33 +1,37 @@
 import React from 'react';
+import avatar from "../assets/avatar-food.png";
+import { AiOutlineMail, RiUserSettingsLine, AiOutlineDelete, HiOutlineLogout } from "react-icons/all";
 
 function SideBar() {
     return (
-        <div>
-            <div className='bg-secondary'>
-                <div>
-                    <img src="" alt="" />
+        <div className=''>
+            <div className='bg-secondary p-5 w-fit'>
+                <div className='flex items-center'>
+                    <img className='w-10 rounded-full object-cover' src={avatar} alt="" />
                     <p>username</p>
                 </div>
                 
-                <div>
-                    <div>
-                        <p>Icon</p>
+                <div className='mt-10'>
+                    <div className='flex items-center gap-2 mb-4'>
+                        <AiOutlineMail size={25} />
                         <p>Messages</p>
                     </div>
-                    <div>
-                        <p>Icon</p>
+                    <div className='flex items-center gap-2 mb-4'>
+                        <RiUserSettingsLine size={25} />
                         <p>Settings</p>
                     </div>
-                    <div>
-                        <p>Icon</p>
+                    <div className='flex items-center gap-2 mb-4'>
+                        <AiOutlineDelete size={25} />
                         <p>Delete account</p>
                     </div>
-                    <div>
-                        <p>Icon</p>
+                    <div className='flex items-center gap-2 mb-4'>
+                        <HiOutlineLogout size={25} />
                         <p>Logout</p>
                     </div>
                 </div>
             </div>
+
+
         </div>
     );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SideBar from '../components/SideBar';
 
 function Home() {
     return (
-        <div>
-            Home
+        <div className='p-10'>
+            <SideBar />
         </div>
     );
 }
