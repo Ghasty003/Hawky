6e33547bfcfcf920f0c0e2fc5197645016d1d0df
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-17 14:09:56 -0400

    Initialized Login page

---
+++ frontend/src/pages/auth/Login.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from "react-router-dom"
+import register from "../../assets/registerchat.png";
+import NavBar from '../../components/NavBar';
+import { AiOutlineUser, RiLockPasswordFill, MdOutlineAlternateEmail } from "react-icons/all";
+
+function Login() {
+    return (
+        <>
+            <NavBar />
+
+            <div className='flex justify-around items-center mt-20'>
+                <div>
+                    <img className='w-[320px]' src={register} alt="register" />
+                    <p className='text-2xl'>Register with us to enjoy realtime chats like no other.</p>
+                </div>
+
+                <div className='bg-navbg text-center w-96 h-[400px] flex flex-col rounded-lg p-6'>
+                    <h2 className='text-2xl font-heading'>Register</h2>
+
+                    <form className='mt-6'>
+                        <div className='flex items-center my-4 bg-gray-900 py-1 rounded-md'>
+                            <p className='h-[40px] text-center flex items-center justify-center p-2'><AiOutlineUser size={20} /></p>
+                            <input className='w-11/12 py-2 px-1 bg-gray-900' type="text" placeholder='Username' />
+                        </div>
+
+                        <div className='flex items-center my-4 bg-gray-900 py-1 rounded-md'>
+                            <p className='h-[40px] text-center flex items-center justify-center p-2'><RiLockPasswordFill size={20} /></p>
+                            <input className='w-11/12 py-2 px-1 bg-gray-900' type="password" placeholder='Password' />
+                        </div>
+
+                        <button className='bg-register text-gray-600 px-4 py-2 rounded-lg font-bold drop-shadow-2xl active:scale-75 duration-300'>
+                            Login
+                        </button>
+                    </form>
+
+                    <h3 className='mt-7'>Don't have an account? <Link className='text-register' to="/login">Register</Link></h3>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Login;
