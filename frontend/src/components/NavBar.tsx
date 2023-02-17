9c6521f7b0344700dd2e80e48fd11c95aedcaa84
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-17 14:24:44 -0400

    updated color

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -5,10 +5,10 @@ import logo from "../assets/hawky.png";
 function NavBar() {
     return (
         <div className='flex justify-around items-center py-6 bg-navbg sticky top-0'>
-            <div className='flex gap-2 items-center'>
+            <Link to="/" className='flex gap-2 items-center'>
                 <img className='w-8 rounded-full object-cover' src={logo} alt="logo" />
                 <h2 className='text-2xl font-heading'>Hawky</h2>
-            </div>
+            </Link>
 
             <div className='flex items-center gap-6'>
                 <button className='bg-login text-gray-600 px-4 py-2 rounded-lg font-bold drop-shadow-2xl active:scale-75 duration-300'>
