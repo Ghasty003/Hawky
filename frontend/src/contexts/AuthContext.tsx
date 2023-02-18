758879ce6d414b550cc1aa412fbf1d000e1ae638
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-18 11:27:27 -0400

    added auth context

---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, useReducer, Reducer } from "react";
+import { AuthActionProp, AuthContextType, AuthReducerStateProp, ProviderProp, Type } from "..";
+
+
+const AuthContext = createContext<AuthContextType>(null!);
+
+
+const authReducer: Reducer<AuthReducerStateProp, AuthActionProp> = (state, action) => {
+    switch (action.type) {
+        case Type.LOGOUT:
+            return {
+                user: null
+            }
+
+        case Type.LOGIN:
+            return {
+                user: action.payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+
+const initialState: AuthReducerStateProp = {
+    user: null
+}
+
+
+export const AuthContextProvider: React.FC<ProviderProp> = ({ children }) => {
+
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
+    return (
+        <AuthContext.Provider value={{ state, dispatch }}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
+
+
+
+export default AuthContext;
