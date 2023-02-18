d0c6f6b01772915c44e204868d72e07ae3319b18
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-18 14:12:45 -0400

    setup auth routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
-import { Route, Routes } from "react-router-dom"
+import { useContext } from "react"
+import { Navigate, Route, Routes } from "react-router-dom"
+import AuthContext from "./contexts/AuthContext"
 import Login from "./pages/auth/Login"
 import Register from "./pages/auth/Register"
 import Home from "./pages/Home"
@@ -6,13 +8,17 @@ import LandingPage from "./pages/LandingPage"
 
 function App() {
 
+  const { state } = useContext(AuthContext);
+
+  const { user } = state;
+
   return (
     <div>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={ user ? <Home /> : <Navigate to="/login" /> } />
+        <Route path="/" element={!user ? <LandingPage /> : <Navigate to="/" /> } />
+        <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
+        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
       </Routes>
     </div>
   )
