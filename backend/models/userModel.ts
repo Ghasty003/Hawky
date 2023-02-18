e317f59e70e26051b95216740be42fd0c77de618
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-18 09:21:26 -0400

    added user model

---
+++ backend/models/userModel.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        unique: true,
+        required: true
+    }
+});
+
+
+export default mongoose.model("users", userSchema);
