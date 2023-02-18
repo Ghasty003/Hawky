97b3b5bd1eeab7a3eb9b1554f73af8d917644812
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-18 08:38:20 -0400

    refactored

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -38,17 +38,6 @@ function ChatBox() {
                 <Messages />
                 <Messages />
             </div>
-
-            <form className='flex bg-white items-center h-14 w-full'>
-                <InputEmoji value={m} onChange={handle}  />
-                {/* <input type="text" placeholder='Type your message...' className='w-[75%] outline-none px-3 h-full text-black' /> */}
-
-                <div className='flex items-center justify-between w-40'>
-                    <img src={img} alt="image" />
-                    {/* <BsEmojiSmile size={25} className='text-black' /> */}
-                    <button className='bg-register px-3 py-1 mr-2 rounded-md'>Send</button>
-                </div>
-            </form>
         </div>
     );
 }
