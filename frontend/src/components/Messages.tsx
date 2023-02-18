e884da285cfdae7b1607691cb95c6482d5ed2cc8
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-17 22:40:49 -0400

    added margin

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Messages() {
     return (
-        <div className='px-2'>
+        <div className='px-2 my-3'>
             <div className='bg-[#3e3c61] w-fit p-1 max-w-[200px] rounded-b-lg rounded-tr-lg'>
                 Messages max-w-[40px] lorem lorem 
             </div>
