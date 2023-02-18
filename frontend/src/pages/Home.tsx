578a375df7d40b1559c904622c60a17ba8834eb1
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-17 20:49:33 -0400

    updated width

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import ChatBox from '../components/ChatBox';
 import SideBar from '../components/SideBar';
 
 function Home() {
     return (
-        <div className='p-10'>
-            <SideBar />
+        <div className='flex justify-center items-center h-screen'>
+            <div className='flex w-[90%]'>
+                <SideBar />
+                <ChatBox />
+            </div>
         </div>
     );
 }
