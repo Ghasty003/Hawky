babf3a72d17f7bbec968d1e6aa74348f60880131
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-18 09:28:54 -0400

    added signup model

--- backend/dist/models/userModel.js
+++ backend/dist/models/userModel.js
@@ -22,6 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importStar(require("mongoose"));
 const userSchema = new mongoose_1.Schema({
@@ -40,4 +49,17 @@ const userSchema = new mongoose_1.Schema({
         required: true
     }
 });
+userSchema.statics.signup = function (email, password, userName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!email || !password || !userName) {
+            throw new Error("All fields are required");
+        }
+        const exists = yield this.findOne({ $or: [{ email }, { userName }] });
+        if (exists) {
+            throw new Error("User already exists");
+        }
+        const user = yield this.create({ email, password, userName });
+        return user;
+    });
+};
 exports.default = mongoose_1.default.model("users", userSchema);
--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -18,4 +18,20 @@ const userSchema = new Schema({
 });
 
 
+userSchema.statics.signup = async function (email: string, password: string, userName: string) {
+    if (!email || !password || !userName) {
+        throw new Error("All fields are required");
+    }
+
+    const exists = await this.findOne({ $or: [{email}, {userName}] });
+
+    if (exists) {
+        throw new Error("User already exists")
+    }
+
+    const user = await this.create({email, password, userName});
+    
+    return user;
+}
+
 export default mongoose.model("users", userSchema);
