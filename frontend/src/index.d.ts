36dfe3fbcbfd7f7d6bf7e79b19887535566491ba
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-18 11:27:17 -0400

    updated types

--- frontend/src/index.d.ts
+++ frontend/src/index.d.ts
@@ -1,5 +1,30 @@
+import React from "react";
+
 interface DivProps {
     headingText: string;
     text: string;
     img: string;
+}
+
+interface ProviderProp {
+    children: React.ReactNode;
+}
+
+interface AuthReducerStateProp {
+    user: object | null;
+}
+
+enum Type {
+    LOGIN = "LOGIN",
+    LOGOUT = "LOGOUT"
+}
+
+interface AuthActionProp {
+    type: Type,
+    payload: object;
+}
+
+interface AuthContextType {
+    state: AuthReducerStateProp;
+    dispatch: React.Dispatch<AuthActionProp>;
 }
