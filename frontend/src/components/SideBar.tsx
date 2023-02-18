cb774f27135ceb33db1ca2c285b06c01724ed304
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-17 20:04:13 -0400

    added chat component

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import avatar from "../assets/avatar-food.png";
 import { AiOutlineMail, RiUserSettingsLine, AiOutlineDelete, HiOutlineLogout, FcSearch } from "react-icons/all";
+import Chats from './Chats';
 
 function SideBar() {
     return (
         <div className='flex items-start'>
-            <div className='bg-[#3e3c61] p-5 w-fit min-h-[500px] rounded-bl-lg rounded-tl-lg'>
+            <div className='bg-[#3e3c61] p-5 w-fit h-[500px] rounded-bl-lg rounded-tl-lg'>
                 <div className='flex items-center gap-2'>
                     <img className='w-10 rounded-full object-cover' src={avatar} alt="" />
                     <p>username</p>
@@ -31,19 +32,29 @@ function SideBar() {
                 </div>
             </div>
 
-            <div className='px-5 py-3 bg-secondary min-h-[500px] h-fit'>
-                <h2 className='text-2xl mb-6'>Messages</h2>
+            <div className='px-5 bg-secondary h-[500px] overflow-auto'>
+                <div className='sticky top-0 bg-secondary'>
+                    <h2 className='text-2xl mb-6 sticky'>Messages</h2>
 
-                <form>
-                    <p className='ml-1'>Find a User</p>
-                    
-                    <div className='bg-primary p-3 rounded-2xl'>
-                        <input type="text" placeholder='Type their username' className='outline-none bg-transparent border-none' />
-                        <button className='bg-secondary text-center rounded-lg p-1 active:scale-90 duration-300'>
-                            <FcSearch size={20} className='inline' />Search
-                        </button>
-                    </div>
-                </form>
+                    <form>
+                        <p className='ml-1'>Find a User</p>
+                        
+                        <div className='bg-primary p-3 rounded-2xl'>
+                            <input type="text" placeholder='Type their username' className='outline-none bg-transparent border-none' />
+                            <button className='bg-secondary text-center rounded-lg p-1 active:scale-90 duration-300'>
+                                <FcSearch size={20} className='inline' />Search
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
+
+                <div className='flex flex-col gap-5 mt-4'>
+                    <Chats />
+                    <Chats />
+                    <Chats />
+                    <Chats />
+                </div>
             </div>
 
         </div>
