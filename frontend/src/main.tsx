d9430c517225a8270123895f0381219a1f12a432
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-19 08:16:42 -0400

    added chatcontext provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthContextProvider } from './contexts/AuthContext'
+import { ChatContextProvider } from './contexts/ChatContext'
 import { FriendContextProvider } from './contexts/FriendContext'
 import './index.css'
 
@@ -10,9 +11,11 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <AuthContextProvider>
       <FriendContextProvider>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
+        <ChatContextProvider>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </ChatContextProvider>
       </FriendContextProvider>
     </AuthContextProvider>
   </BrowserRouter>,
