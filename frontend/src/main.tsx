e1d2bbe1f1c0220a1b0eab22f4f4663b8f642840
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-19 06:40:30 -0400

    added friend context provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthContextProvider } from './contexts/AuthContext'
+import { FriendContextProvider } from './contexts/FriendContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <AuthContextProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <FriendContextProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </FriendContextProvider>
     </AuthContextProvider>
   </BrowserRouter>,
 )
