304350c0c1fdda35fd885547a094483ccc752724
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-19 10:17:00 -0400

    displays text messages

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { MessagesProp } from '../types';
 
-function Messages() {
+function Messages({ message }: {message: MessagesProp}) {
+
     return (
         <div className='px-2 my-3'>
             <div className='bg-[#3e3c61] w-fit p-1 max-w-[200px] rounded-b-lg'>
-                Messages max-w-[40px] lorem lorem 
+                { message.text }
             </div>
         </div>
     );
