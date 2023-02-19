828896aa90ffa283967bd6090415bb787ce75c7f
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-19 10:16:47 -0400

    map messages array

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import MessageContext from "../contexts/MessageContext";
 import Header from "./Header";
 import Input from "./Input";
 import Messages from './Messages';
@@ -5,23 +7,18 @@ import Messages from './Messages';
 
 function ChatBox() {
 
+    const { messages } = useContext(MessageContext);
+
     return (
         <div className='w-[50%] relative'>
             <Header />
 
             <div className='custom-height bg-empty-chat overflow-auto'>
-                <Messages />
-                <Messages />
-                <Messages />
-                <Messages />
-                <Messages />
-                <Messages />
-                <Messages />
-                <Messages />
-                <Messages />
-                <Messages />
-                <Messages />
-                <Messages />
+                {
+                    messages.map(message => (
+                        <Messages key={message._id} message={message} />
+                    ))
+                }
             </div>
 
             <Input />
