373e02b75291fc9ced51e2b6ab85906fde32f005
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-19 13:12:27 -0400

    added online user types

--- frontend/src/contexts/SocketContext.tsx
+++ frontend/src/contexts/SocketContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useRef, useContext } from "react";
+import React, { createContext, useEffect, useRef, useContext, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { ProviderProp, User } from "../types";
 import AuthContext from "./AuthContext";
@@ -13,6 +13,7 @@ const SocketContext = createContext<test>(null!);
 export const SocketContextProvider: React.FC<ProviderProp> = ({ children }) => {
 
     const socket = useRef<Socket>(null!);
+    const [onlineUser, setOnlineUser] = useState([]);
 
     const { state } = useContext(AuthContext);
     const { user } = state;
@@ -24,6 +25,9 @@ export const SocketContextProvider: React.FC<ProviderProp> = ({ children }) => {
     useEffect(() => {
         socket.current = io("http://localhost:3000");
         socket.current.emit("add-new-user", currentUser?.id);
+        socket.current.on("get-users", (activeUsers) => {
+            setOnlineUser(activeUsers);
+        })
     }, [currentUser]);
 
 
