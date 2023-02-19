4eab01885a4d79aef95847114f157ecbf741e755
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-19 08:15:52 -0400

    sends message to database

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,23 +1,61 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import InputEmoji from "react-input-emoji";
 import img from "../assets/img.png";
+import AuthContext from '../contexts/AuthContext';
+import ChatContext from '../contexts/ChatContext';
+import { User } from '../types';
 
 function Input() {
 
-    const [message, setMessage] = useState<string>("");
+    const [text, setText] = useState<string>("");
 
-    const handleChange = (message: string) => {
-        setMessage(message);
+    const { state } = useContext(AuthContext);
+    const { chat } = useContext(ChatContext);
+
+    const { user } = state;
+    const currentUser = user as User;
+
+    const handleChange = (text: string) => {
+        setText(text);
+    }
+
+    const handleSend = async (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        const body = {
+            senderId: currentUser._id,
+            receiverId: chat.friendDetails.friendId,
+            text
+        }
+    
+        const res = await fetch("http://localhost:3000/api/messages", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+        });
+
+        const json = await res.json();
+
+        if (!res.ok) {
+            console.log(json.error)
+        }
+
+        if (res.ok) {
+            console.log(json);
+        // setMessage("");
+        // socket.current.emit("send-message", json);
+        // setMessages(prev => [...prev, json])
+        }
     }
 
     return (
-        <form className='flex bg-white items-center h-14 w-full'>
-            <InputEmoji value={message} onChange={handleChange}  />
-            {/* <input type="text" placeholder='Type your message...' className='w-[75%] outline-none px-3 h-full text-black' /> */}
+        <form onSubmit={handleSend} className='flex bg-white items-center h-14 w-full'>
+            <InputEmoji value={text} onChange={handleChange}  />
 
             <div className='flex items-center justify-between w-40'>
                 <img src={img} alt="image" />
-                {/* <BsEmojiSmile size={25} className='text-black' /> */}
                 <button className='bg-register px-3 py-1 mr-2 rounded-md'>Send</button>
             </div>
         </form>
