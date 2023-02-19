09db216297fbff6b084843af17412a28b9303d84
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-19 16:46:28 -0400

    added notification text when no chat

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { Socket } from "socket.io-client";
+import ChatContext from "../contexts/ChatContext";
 import MessageContext from "../contexts/MessageContext";
 import SocketContext from "../contexts/SocketContext";
 import Header from "./Header";
@@ -10,6 +11,7 @@ import Messages from './Messages';
 function ChatBox({ socket}: {socket: React.MutableRefObject<Socket>}) {
 
     const { messages, setMessages } = useContext(MessageContext);
+    const { chat } = useContext(ChatContext);
     
 
     return (
@@ -18,9 +20,9 @@ function ChatBox({ socket}: {socket: React.MutableRefObject<Socket>}) {
 
             <div className='custom-height bg-empty-chat overflow-auto'>
                 {
-                    messages.map(message => (
+                    chat ? messages.map(message => (
                         <Messages key={message._id} message={message} />
-                    ))
+                    )) : <div className="h-full flex justify-center items-center text-2xl">Tap on a conversation to start Chatting</div>
                 }
             </div>
 
