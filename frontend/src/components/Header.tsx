ed841d7facdaa7f3ed716d2f4e0250318e29f6da
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-19 08:27:27 -0400

    displays friend username on header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,23 @@
+import { useContext } from "react";
 import videocall from "../assets/cam.png";
 import more from "../assets/more.png";
+import AuthContext from "../contexts/AuthContext";
+import ChatContext from "../contexts/ChatContext";
+import { User } from "../types";
 
 function Header() {
+
+    const { state } = useContext(AuthContext);
+    const { chat } = useContext(ChatContext);
+
+    const { user } = state;
+    const currentUser = user as User;
+
+    const isMyUsername = chat?.friendDetails.friendUsername === currentUser.userName;
+
     return (
         <div className='bg-[#3e3c61] flex justify-between items-center h-10 px-2'>
-            <p>Username</p>
+            <p>{ isMyUsername ? chat.friendDetails.userName : chat?.friendDetails.friendUsername }</p>
 
             <div className='flex items-center'>
                 <img className='w-7' src={videocall} alt="" />
