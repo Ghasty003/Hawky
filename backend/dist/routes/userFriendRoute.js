fd50225bfb42b50e3975b270b7b2823b84eb9440
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-18 22:20:13 -0400

    added get friends function

--- backend/dist/routes/userFriendRoute.js
+++ backend/dist/routes/userFriendRoute.js
@@ -9,4 +9,5 @@ const requireAuth_1 = __importDefault(require("../middlewares/requireAuth"));
 const router = (0, express_1.Router)();
 router.use(requireAuth_1.default);
 router.post("/addFriend", userFriendController_1.addFriend);
+router.get("/", userFriendController_1.getFriends);
 exports.default = router;
--- backend/routes/userFriendRoute.ts
+++ backend/routes/userFriendRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addFriend } from "../controllers/userFriendController";
+import { addFriend, getFriends } from "../controllers/userFriendController";
 import requireAuth from "../middlewares/requireAuth";
 
 
@@ -8,6 +8,8 @@ const router = Router();
 router.use(requireAuth);
 
 router.post("/addFriend", addFriend);
+
+router.get("/", getFriends);
 
 
 export default router;
