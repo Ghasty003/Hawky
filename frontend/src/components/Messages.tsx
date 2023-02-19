f914017e02c04d98ab8e0350da53ae6c0d1abc0a
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-19 10:23:35 -0400

    added owner condition check

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import { MessagesProp } from '../types';
+import React, { useContext } from 'react';
+import AuthContext from '../contexts/AuthContext';
+import { MessagesProp, User } from '../types';
 
 function Messages({ message }: {message: MessagesProp}) {
 
+    const { state } = useContext(AuthContext);
+
+    const { user } = state;
+    const currentUser = user as User;
+
+    const isOwner = message.senderId === currentUser.id;
+    console.log(isOwner)
+
     return (
-        <div className='px-2 my-3'>
-            <div className='bg-[#3e3c61] w-fit p-1 max-w-[200px] rounded-b-lg'>
+        <div className={`${isOwner ? "owner" : "not-owner"} px-2 my-3`}>
+            <div className={`bg-[#3e3c61] w-fit p-1 max-w-[200px] rounded-b-lg`}>
                 { message.text }
             </div>
         </div>
