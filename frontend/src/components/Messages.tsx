70537e299f5b6b8a617064ecdce1599f5e9fc0a8
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-19 14:08:14 -0400

    scrolls message into view

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -1,9 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import AuthContext from '../contexts/AuthContext';
+import MessageContext from '../contexts/MessageContext';
 import { MessagesProp, User } from '../types';
 
 function Messages({ message }: {message: MessagesProp}) {
 
+    const div = useRef<HTMLDivElement>(null!);
+
+    const { messages } = useContext(MessageContext);
     const { state } = useContext(AuthContext);
 
     const { user } = state;
@@ -11,8 +15,12 @@ function Messages({ message }: {message: MessagesProp}) {
 
     const isOwner = message.senderId === currentUser.id;
 
+    React.useEffect(() => {
+        div.current.scrollIntoView({behavior: "smooth"});
+    }, [ messages ]);
+
     return (
-        <div className={`${isOwner ? "owner" : "not-owner"} px-2 my-3`}>
+        <div ref={div} className={`${isOwner ? "owner" : "not-owner"} px-2 my-3`}>
             <div className={`${isOwner ? "owner" : "not-owner"} bg-[#3e3c61] w-fit p-1 max-w-[200px] rounded-b-lg`}>
                 { message.text }
             </div>
