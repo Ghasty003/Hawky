e51a5d607f1f6db9f623886e0f2f3ba1eba6101e
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-21 20:52:25 -0400

    added loading state

--- frontend/src/contexts/MessageContext.tsx
+++ frontend/src/contexts/MessageContext.tsx
@@ -8,9 +8,10 @@ const MessageContext = createContext<MessagesContextType>(null!);
 export const MessageContextProvider: React.FC<ProviderProp> = ({ children }) => {
 
     const [messages, setMessages] = useState<MessagesProp[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
 
     return (
-        <MessageContext.Provider value={{messages, setMessages}}>
+        <MessageContext.Provider value={{messages, setMessages, loading}}>
             { children }
         </MessageContext.Provider>
     )
