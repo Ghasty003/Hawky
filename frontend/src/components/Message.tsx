9d0140851fde698c1f5fdebe11e3d05c1682491e
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-26 17:22:21 -0400

    updated paddings

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -89,7 +89,7 @@ function Message({ setFriend, friend }: Props) {
     }
 
     return (
-        <div className='px-5 bg-secondary h-[500px] overflow-auto'>
+        <div className='mobile:px-3 px-5 bg-secondary h-[500px] overflow-auto'>
             <div className='sticky top-0 bg-secondary'>
                 
                 <div className='mb-6 sticky mt-1 flex justify-between items-center'>
