e0fbe6948dfee387753bd8764524bbef53571184
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-26 15:16:39 -0400

    passes down socket and added chatbox route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
-import { useContext } from "react"
+import { useContext, useRef } from "react"
 import { Route, Routes, Navigate } from "react-router-dom"
+import { Socket } from "socket.io-client"
+import ChatBox from "./components/ChatBox"
 import AuthContext from "./contexts/AuthContext"
 import Login from "./pages/auth/Login"
 import Register from "./pages/auth/Register"
@@ -12,13 +14,16 @@ function App() {
 
   const { user } = state;
 
+  const socket = useRef<Socket>(null!);
+
   return (
     <div>
       <Routes>
-        <Route path="/home" element={user ? <Home /> : <Navigate to="/login" /> } />
+        <Route path="/home" element={user ? <Home socket={socket} /> : <Navigate to="/login" /> } />
         <Route path="/" element={!user ? <LandingPage /> : <Navigate to="/home" />} />
         <Route path="/register" element={!user ? <Register /> : <Navigate to="/home" />} />
         <Route path="/login" element={!user ? <Login /> : <Navigate to="/home" />} />
+        <Route path="/chat" element={<ChatBox socket={socket} />} />
       </Routes>
     </div>
   )
